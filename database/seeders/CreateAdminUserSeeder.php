e7f900bd0dd1146bbda10273685fb7d3a4032490
esraaeissa123 <esraa.eissa123@gmail.com>
2022-02-23 12:13:53 +0200

    commit

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -1,33 +1,36 @@
 <?php
   
 namespace Database\Seeders;
-  
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-  
 class CreateAdminUserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $user = User::create([
-            'name' => 'Hardik Savani', 
-            'email' => 'admin@gmail.com',
-            'password' => bcrypt('123456')
-        ]);
+/**
+* Run the database seeds.
+*
+* @return void
+*/
+public function run()
+{
     
-        $role = Role::create(['name' => 'Admin']);
-     
-        $permissions = Permission::pluck('id','id')->all();
+         $user = User::create([
+        'name' => 'Mr.Waleed', 
+        'email' => 'walid.purelife@gmail.com',
+        'password' => bcrypt('123456'),
+        'roles_name' => ["owner"],
+        'Status' => 'مفعل',
+        ]);
+  
+        $role = Role::create(['name' => 'owner']);
    
+        $permissions = Permission::pluck('id','id')->all();
+  
         $role->syncPermissions($permissions);
-     
+   
         $user->assignRole([$role->id]);
-    }
+
+
+}
 }
--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,56 +1,136 @@
-@extends('layouts.app')
+@extends('layouts.master')
+@section('css')
+    <!--Internal   Notify -->
+    <link href="{{ URL::asset('assets/plugins/notify/css/notifIt.css') }}" rel="stylesheet" />
+@section('title')
+    صلاحيات المستخدمين - بيورلايف
+@stop
 
 
-@section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Role Management</h2>
-        </div>
-        <div class="pull-right">
-        @can('role-create')
-            <a class="btn btn-success" href="{{ route('roles.create') }}"> Create New Role</a>
-            @endcan
+@endsection
+@section('page-header')
+<!-- breadcrumb -->
+<div class="breadcrumb-header justify-content-between">
+    <div class="my-auto">
+        <div class="d-flex">
+            <h4 class="content-title mb-0 my-auto">المستخدمين</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0"> /
+                صلاحيات المستخدمين</span>
         </div>
     </div>
 </div>
+<!-- breadcrumb -->
+@endsection
+@section('content')
 
 
-@if ($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
-    </div>
+@if (session()->has('Add'))
+    <script>
+        window.onload = function() {
+            notif({
+                msg: " تم اضافة الصلاحية بنجاح",
+                type: "success"
+            });
+        }
+
+    </script>
 @endif
 
+@if (session()->has('edit'))
+    <script>
+        window.onload = function() {
+            notif({
+                msg: " تم تحديث بيانات الصلاحية بنجاح",
+                type: "success"
+            });
+        }
 
-<table class="table table-bordered">
-  <tr>
-     <th>No</th>
-     <th>Name</th>
-     <th width="280px">Action</th>
-  </tr>
-    @foreach ($roles as $key => $role)
-    <tr>
-        <td>{{ ++$i }}</td>
-        <td>{{ $role->name }}</td>
-        <td>
-            <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
-            @can('role-edit')
-                <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
-            @endcan
-            @can('role-delete')
-                {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                {!! Form::close() !!}
-            @endcan
-        </td>
-    </tr>
-    @endforeach
-</table>
+    </script>
+@endif
+
+@if (session()->has('delete'))
+    <script>
+        window.onload = function() {
+            notif({
+                msg: " تم حذف الصلاحية بنجاح",
+                type: "error"
+            });
+        }
+
+    </script>
+@endif
+
+<!-- row -->
+<div class="row row-sm">
+    <div class="col-xl-12">
+        <div class="card">
+            <div class="card-header pb-0">
+                <div class="d-flex justify-content-between">
+                    <div class="col-lg-12 margin-tb">
+                        <div class="pull-right">
+                            @can('اضافة صلاحية')
+                                <a class="btn btn-primary btn-sm" href="{{ route('roles.create') }}">اضافة</a>
+                            @endcan
+                        </div>
+                    </div>
+                    <br>
+                </div>
+
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table mg-b-0 text-md-nowrap table-hover ">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>الاسم</th>
+                                <th>العمليات</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($roles as $key => $role)
+                                <tr>
+                                    <td>{{ ++$i }}</td>
+                                    <td>{{ $role->name }}</td>
+                                    <td>
+                                        @can('عرض صلاحية')
+                                            <a class="btn btn-success btn-sm"
+                                                href="{{ route('roles.show', $role->id) }}">عرض</a>
+                                        @endcan
+                                        
+                                        @can('تعديل صلاحية')
+                                            <a class="btn btn-primary btn-sm"
+                                                href="{{ route('roles.edit', $role->id) }}">تعديل</a>
+                                        @endcan
+
+                                        @if ($role->name !== 'owner')
+                                            @can('حذف صلاحية')
+                                                {!! Form::open(['method' => 'DELETE', 'route' => ['roles.destroy',
+                                                $role->id], 'style' => 'display:inline']) !!}
+                                                {!! Form::submit('حذف', ['class' => 'btn btn-danger btn-sm']) !!}
+                                                {!! Form::close() !!}
+                                            @endcan
+                                        @endif
 
 
-{!! $roles->render() !!}
-
-
-<p class="text-center text-primary"><small>Tutorial by ItSolutionStuff.com</small></p>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--/div-->
+</div>
+<!-- row closed -->
+</div>
+<!-- Container closed -->
+</div>
+<!-- main-content closed -->
+@endsection
+@section('js')
+<!--Internal  Notify js -->
+<script src="{{ URL::asset('assets/plugins/notify/js/notifIt.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/notify/js/notifit-custom.js') }}"></script>
 @endsection
